0b5b65798bee146b94a0b8c4ae1298f6c73381d1
Landrey Kiker <42449999+LKiker@users.noreply.github.com>
2026-07-24 20:43:48 +0000

    config update for github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://lkiker.github.io',
+  // no `base` needed for a username.github.io repo
+});
