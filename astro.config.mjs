21f692ea42c347c2af78b026ce1d55ddbc3d40f0
Landrey Kiker <42449999+LKiker@users.noreply.github.com>
2026-07-24 20:46:04 +0000

    added deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://lkiker.github.io',
-  // no `base` needed for a username.github.io repo
-});
+export default defineConfig({});
